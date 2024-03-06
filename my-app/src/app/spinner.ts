c750efc4c6bc82a2e0bd003eb928b47f4c8fd4ec
Pedro Hurtado <pedrohurt@gmail.com>
2024-03-06 08:20:39 +0000

    routes

--- my-app/src/app/spinner.ts
+++ my-app/src/app/spinner.ts
@@ -1,9 +1,10 @@
 export function Spinner() {
     return  (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
        const oldFunction:Function = descriptor.value as Function;
-       descriptor.value = async (...args:[])=>{          
+       descriptor.value = async function (...args:[]){          
+          console.log(this)
           try{
-            return await oldFunction.apply(target,args)
+            return await oldFunction.apply(this,args)
           }
           catch{
                 console.log("error")
